ce2a42f5b092b55704e85464a6a98672c35e4b42
venugopalachari <venugopalachary8686@gmail.com>
2021-11-21 11:40:48 +0530

    Update import.php

--- import.php
+++ import.php
@@ -1,9 +1,3 @@
 <?php
-global $_POST;
-$email = isset( $_POST['email'] ) ? $_POST['email'] : '';
-if ( ! empty ( $email ) ) {
-	$file    = 'email.csv';
-	$current = file_get_contents( $file );
-	$current .= $email . ';' . '\n';
-	file_put_contents( $file, $current );
-}
+echo "helloworld";
+?>
